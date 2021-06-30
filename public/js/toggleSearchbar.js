b513d5ee21424f6180a35ea12394e8e015488d18
winnie-webb <wbrown152020@gmail.com>
2021-06-29 20:47:25 -0500

    Added full responsiveness to header

--- public/js/toggleSearchbar.js
+++ public/js/toggleSearchbar.js
@@ -2,6 +2,7 @@ function toggleSearchBar() {
   // Toggle searching
   const searchBarWrapper = document.querySelector(".search__barWrapper");
   const searchBar = document.querySelector(".search__bar");
+  const searchBarMobile = document.querySelector(".searchBtn-mobile");
   // Selecting  Togglers
   const searchIconToggler = document.querySelector(".search__barIcon__wrapper");
   const searchBarCloser = document.querySelector(".search__barCloseMenu");
@@ -13,6 +14,8 @@ function toggleSearchBar() {
     headerLogo.classList.toggle("hidden");
     burgerMenu.classList.toggle("hidden");
     searchBarCloser.classList.toggle("hidden");
+    searchIconToggler.classList.toggle("hidden");
+    searchBarMobile.classList.toggle("hidden");
     searchBarWrapper.style.display = "flex";
   }
   searchIconToggler.addEventListener("click", () => {
@@ -21,7 +24,7 @@ function toggleSearchBar() {
   });
 
   //   Toggle not searching
-  const closeSearchBar = document.querySelector(".close-menu");
+  const closeSearchBar = document.querySelector(".search__barCloseMenu");
   closeSearchBar.addEventListener("click", () => {
     toggleElements();
     searchBar.style.display = "none";
